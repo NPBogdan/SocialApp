dc76e486b8ffb549085614cf329aafee0b8e5342
vagrant <vagrant@homestead>
2022-09-04 17:29:42 +0000

    First commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+        host: '192.168.56.56',
+        watch: {
+            usePolling: true,
+        },
+    },
+});
